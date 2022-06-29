823e0ca7be75e61b3e9616fdd03dd3a56346f1cc
Shamim Khaled <71558919+shamimkhaled@users.noreply.github.com>
2022-06-30 01:29:01 +0600

    Update TextForm.js

--- TextAnalyzer/src/components/TextForm.js
+++ TextAnalyzer/src/components/TextForm.js
@@ -27,6 +27,11 @@ export default function TextForm(props) {
       navigator.clipboard.writeText(textCopy.value);
       props.showAlert("Copied to the clipboard", "Success")
    }
+   const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces removed!", "success");
+}
    const handleTextClearClick = ()=>{
     // console.log('Uppercase was clicked!', +text);
      let newText= '';
@@ -45,17 +50,18 @@ export default function TextForm(props) {
       <div className="mb-3">
         <textarea className="form-control" value={text} style={{ backgroundColor: props.mode==='dark'?'#212529':'white', color: props.mode==='dark'?'white':'black' }} onChange={handleOnChange}   id="textBox" rows="8" ></textarea>
       </div>
-      <button className="btn btn-info mx-3" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleUppClick}>Convert to Uppercase</button>
-      <button className="btn btn-info mx-3" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleLowClick}>Convert to Lowercase</button>
-      <button className="btn btn-info mx-3" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleCapClick}>Capitilized Case</button>
-      <button className="btn btn-info mx-3" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleTextCopy}>Copy Text</button>
-      <button className="btn btn-info mx-3" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleTextClearClick}>Clear Text</button>
+      <button disabled={text.length===0} className="btn btn-info mx-2 my-2" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleUppClick}>Convert to Uppercase</button>
+      <button disabled={text.length===0} className="btn btn-info mx-2 my-2" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleLowClick}>Convert to Lowercase</button>
+      <button disabled={text.length===0} className="btn btn-info mx-2 my-2" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleCapClick}>Capitilized Case</button>
+      <button disabled={text.length===0} className="btn btn-info mx-2 my-2" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleTextCopy}>Copy Text</button>
+      <button disabled={text.length===0} className="btn btn-info mx-2 my-2" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+      <button disabled={text.length===0} className="btn btn-info mx-2 my-2" style={{ color: props.mode==='dark'?'white':'black' }} onClick={handleTextClearClick}>Clear Text</button>
     </div>
 
     <div className="container my-2" style={{ color: props.mode==='dark'?'white':'black' }}>
         <h6>Your text summary</h6>
-        <p>{text.split(" ").length-1} words and {text.length} characters and {text.split(/[".!?"]/).length-1} sentences </p>
-        <p>{0.008*text.split(" ").length} minutes to read</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters and {text.split(/[".!?"]/).length-1} sentences </p>
+        <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} minutes to read</p>
         <h4>Preview</h4>
         <p>{text.length>0?text:'Enter something in the above texbox to preview it here.'}</p>
     </div>
